16c8c88f527bdaa62c2995da40c6bcc06ba7e34c
Diogo Krüger <diogoks2005@gmail.com>
2026-01-09 04:02:47 -0300

    fuçando no pthreads

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,13 @@ int main(void){
     pthread_t thread1;
     pthread_create(&thread1, NULL, printOi, NULL);
 
-        pthread_t thread2;
+    pthread_t thread2;
     pthread_create(&thread2, NULL, printOi, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
-        std::cout << "cheguei aqui" << std::endl;
+    std::cout << "cheguei aqui" << std::endl;
 
     return 0;
 }
